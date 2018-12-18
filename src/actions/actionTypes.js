91875d860ce4173c2394564de4a12ab4dc54e5fd
Paolo Holganza <paolo.holganza@gmail.com>
2018-12-18 15:06:30 -0500

    remove unused code

--- src/actions/actionTypes.js
+++ src/actions/actionTypes.js
@@ -9,9 +9,5 @@ export const ActionTypes = keyMirror({
   ALBUMS_LOAD_FAILED: null,
   PICTURES_LOAD: null,
   PICTURES_LOAD_COMPLETED: null,
-  PICTURES_LOAD_FAILED: null,
-  NAVIGATE_TO_HOME: null,
-  NAVIGATE_TO_USER: null,
-  NAVIGATE_TO_ALBUM: null,
-  NAVIGATE_TO_PICTURE: null
+  PICTURES_LOAD_FAILED: null
 })
